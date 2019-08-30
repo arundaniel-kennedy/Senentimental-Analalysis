d3d8bb44d0d119c83f71fbf1b7c010e218f8c2d6
arundaniel-kennedy <arundanielkennedy@gmail.com>
2019-08-30 12:31:38 +0530

    ad

--- senmovp/routes.py
+++ senmovp/routes.py
@@ -1,11 +1,12 @@
 from flask import Flask,render_template
 import mysql.connector
+import json
 
 config = {
   'user': 'root',
   'password': 'root',
   'host': 'localhost',
-  'database': 'movies',
+  'database': 'movie',
   'raise_on_warnings': True,
 }
 
@@ -19,44 +20,54 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/genre')
+def genre():
+    return render_template("genre.html")
+
 @app.route('/highrate')
 def highrate():
-    mycursor.execute("SELECT * FROM ads order by imdb desc limit 50")
+    mycursor.execute("SELECT * FROM movies order by vote_average desc limit 50")
     myresult = mycursor.fetchall()
 
-    return render_template("highrate.html",result=myresult)
+    return render_template("cards.html",result=myresult)
 
 @app.route('/alphabet')
 def alpha():
-    mycursor.execute("SELECT * FROM ads order by title limit 50")
+    mycursor.execute("SELECT * FROM movies order by title limit 50")
     myresult = mycursor.fetchall()
 
-    return render_template("alpha.html",result=myresult)
+    return render_template("cards.html",result=myresult)
 
 @app.route('/year')
 def year():
-    mycursor.execute("SELECT * FROM ads limit 50")
+    mycursor.execute("SELECT * FROM movies order by year desc limit 50")
     myresult = mycursor.fetchall()
 
-    return render_template("year.html",result=myresult)
+    return render_template("cards.html",result=myresult)
 
 @app.route('/content/<id>')
 def contentpage(id):
-    mycursor.execute("SELECT id FROM review ORDER BY RAND() limit 20")
-    rate = mycursor.fetchall()
+    mycursor.execute("SELECT comment FROM movies WHERE id = '"+id+"'")
+    comment = mycursor.fetchall()
 
     sum=0
-    for y in rate:
-        sum+=y[0]
+    comments=[]
+
+    data = json.loads(comment[0][0])
+    for c in data:
+        mycursor.execute("SELECT id FROM review WHERE slno = '"+str(c)+"'")
+        comment = mycursor.fetchone()
+        sum+=int(comment[0])
+        mycursor.execute("SELECT comment FROM review WHERE slno = '"+str(c)+"'")
+        comme = mycursor.fetchone()
+        comments.append(comme[0])
 
     avg = (sum/20)*100
 
-    #mycursor.execute("UPDATE `ads` SET `rating` = '"+str(avg)+"' WHERE `ads`.`tid` = '"+id+"'")
-    #link.commit()
-
-    mycursor.execute("SELECT * FROM ads WHERE tid = '"+id+"'")
+    mycursor.execute("SELECT * FROM movies WHERE id = '"+id+"'")
     myresult = mycursor.fetchone()
-    return render_template("contentpage.html",x=myresult,rate=avg)
+
+    return render_template("contentpage.html",x=myresult,rate=format(round(avg,2)),comments=comments)
 
 
 if __name__ == "__main__":
