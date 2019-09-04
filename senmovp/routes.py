158f2d35df8e2fc7fb525910cb61b366d535bdd3
arundaniel-kennedy <arundanielkennedy@gmail.com>
2019-09-04 20:01:18 +0530

    commit

--- senmovp/routes.py
+++ senmovp/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import mysql.connector
 import json
 
@@ -18,11 +18,71 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    mycursor.execute("SELECT * FROM movies order by year desc limit 5")
+    myresult = mycursor.fetchall()
 
-@app.route('/genre')
-def genre():
-    return render_template("genre.html")
+    return render_template("index.html",result=myresult)
+
+@app.route('/browse')
+def browse():
+    mycursor.execute("SELECT * FROM movies limit 50")
+    myresult = mycursor.fetchall()
+
+    return render_template("cards.html",result=myresult)
+
+@app.route('/search/<name>')
+def searchbox(name):
+    mycursor.execute("SELECT id,title FROM movies where title like '"+name+"%' limit 20")
+    myresult = mycursor.fetchall()
+
+    return render_template("search.html",result=myresult)
+
+@app.route('/result',methods=['POST'])
+def search():
+    name = request.form['name']
+    name = name.split('(')
+    id = name[-1].split(')')
+
+    #return str(id[0])
+
+    mycursor.execute("SELECT comment FROM movies WHERE id = '"+id[0]+"'")
+    comment = mycursor.fetchall()
+
+    sum=0
+    comments=[]
+
+    data = json.loads(comment[0][0])
+    for c in data:
+        mycursor.execute("SELECT id FROM review WHERE slno = '"+str(c)+"'")
+        comment = mycursor.fetchone()
+        sum+=int(comment[0])
+        mycursor.execute("SELECT comment FROM review WHERE slno = '"+str(c)+"'")
+        comme = mycursor.fetchone()
+        comments.append(comme[0])
+
+    avg = (sum/20)*100
+
+    mycursor.execute("SELECT * FROM movies WHERE id = '"+id[0]+"'")
+    myresult = mycursor.fetchone()
+
+    return render_template("contentpage.html",x=myresult,rate=format(round(avg,2)),comments=comments)
+
+@app.route('/genres')
+def genres():
+    mycursor.execute("SELECT * FROM genre")
+    myresult = mycursor.fetchall()
+    return render_template("genre.html",result=myresult)
+
+@app.route('/genre/<id>')
+def genre(id):
+    mycursor.execute("SELECT * FROM movies")
+    myresult = mycursor.fetchall()
+    ch=[]
+    for i in myresult:
+        if(id in i[12]):
+            ch.append(i)
+    return render_template("cards.html",result=ch)
+
 
 @app.route('/highrate')
 def highrate():
